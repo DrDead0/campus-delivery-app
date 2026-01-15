ba130ff07b4148e4f2a4bf785c753153ab568bf9
rhita-ui <rhitamraychowdhuri626@gmail.com>
2026-01-15 19:54:01 +0530

    commit change in srcoller

--- lib/data.ts
+++ lib/data.ts
@@ -10,58 +10,10 @@ export const hostels = [
   "NC 4 Hostel",
   "NC 2 Hostel",
   "NC 3 Hostel",
-  "NC Hostel",
+  "NC 2 Hostel",
+  "NC 1 Hostel",
 ];
 
 export const categories = [
-  {
-    name: "Burgers",
-    image:
-      "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3",
-  },
-  {
-    name: "Pizza",
-    image:
-      "https://images.unsplash.com/photo-1513104890138-7c749659a591?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3",
-  },
-  {
-    name: "Biryani",
-    image:
-      "https://images.unsplash.com/photo-1589302168068-964664d93dc0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3",
-  },
-  {
-    name: "Chinese",
-    image:
-      "https://images.unsplash.com/photo-1585032226651-759b368d7246?w=500&auto=format&fit=crop&q=60",
-  },
-  {
-    name: "South Indian",
-    image:
-      "https://images.unsplash.com/photo-1589301760014-d929f3979dbc?w=500&auto=format&fit=crop&q=60",
-  },
-  {
-    name: "Rolls",
-    image:
-      "https://images.unsplash.com/photo-1626700051175-6818013e1d4f?w=500&auto=format&fit=crop&q=60",
-  },
-  {
-    name: "Desserts",
-    image:
-      "https://images.unsplash.com/photo-1563729784474-d77dbb933a9e?w=500&auto=format&fit=crop&q=60",
-  },
-  {
-    name: "Coffee",
-    image:
-      "https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?w=500&auto=format&fit=crop&q=60",
-  },
-  {
-    name: "Thali",
-    image:
-      "https://images.unsplash.com/photo-1546833999-b9f581a1996d?w=500&auto=format&fit=crop&q=60",
-  },
-  {
-    name: "Drinks",
-    image:
-      "https://images.unsplash.com/photo-1543573852-1a71a6ce19bc?w=500&auto=format&fit=crop&q=60",
-  },
+ 
 ];
